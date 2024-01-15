d2c26bf8cb1b0b105b6f9aa77274bfa312033873
Abdul Basit <abdulbasit10397@gmail.com>
2024-01-15 17:19:18 +0500

    PF-02 python conditionals

---
+++ fundamentals/conditionals.py
@@ -0,0 +1,8 @@
+#The program contains code written to practice python's conditions
+num1 = 10
+num2 = 20
+
+if num1 > num2:
+    print("First number is greater")
+else:
+    print("Second number is greater")
