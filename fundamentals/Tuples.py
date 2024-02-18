09e80d25d6f264a1dd2b49670e68c9c1b307418d
Abdul Basit <abdulbasit10397@gmail.com>
2024-02-18 15:33:39 +0500

    PF07 - Working with tuples, functions

---
+++ fundamentals/Tuples.py
@@ -0,0 +1,4 @@
+# The main difference between a list and a tuple is that list is mutable but tuple is immutable
+
+coordinates = (1.653636, 23.3534533)
+print(coordinates)
---
+++ fundamentals/functions/SimpleFunctions.py
@@ -0,0 +1,25 @@
+# A simple function with no parameter
+def simple_function():
+    print("Hello from simple function")
+
+
+simple_function()
+
+print("\n******************************************\n")
+
+
+def function_with_1param(name):
+    print("Hello " + name)
+
+
+function_with_1param("Abdul Basit")
+
+
+print("\n******************************************\n")
+
+
+def function_with_2params(name, age):
+    print("Hello " + name + ". You are " + str(age) + " years old.")
+
+
+function_with_2params("Abdul Basit", 27)
