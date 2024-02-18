441a5ef3835869d9313571c75d85d08724a4dfc0
Abdul Basit <abdulbasit10397@gmail.com>
2024-02-18 16:02:00 +0500

    PF10 - Working with conditionals or and & not

--- fundamentals/conditionals/IfElse.py
+++ fundamentals/conditionals/IfElse.py
@@ -18,3 +18,11 @@ elif is_male or is_tall:
     print("You are either male or tall")
 else:
     print("You are neither male nor tall")
+
+print("\n----------------------------------------------------\n")
+numbers = [3, 54, 2, 3, 7, 86, 56]
+key = 54
+if not (numbers.__contains__(key)):
+    print("List doesn't contain: " + str(key))
+else:
+    print("List contains: " + str(key))
