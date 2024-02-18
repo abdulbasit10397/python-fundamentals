8e076c9d0e6cfec72295c2287684a48181b8562f
Abdul Basit <abdulbasit10397@gmail.com>
2024-02-18 16:44:01 +0500

    PF12 - Iterations - For Loop

---
+++ concepts/iterations/ForLoop.py
@@ -0,0 +1,9 @@
+def print_all_numbers(numbers):
+    for n in numbers:
+        print(n, end=" ")
+
+
+numbers = [10, 45, 32, 56, 76, 87, 98, 23, 67]
+
+print("******* ITERATING LIST ELEMENTS")
+print_all_numbers(numbers)
