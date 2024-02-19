b9f8f5dcc5dcb5abb3f539ea714bcd2bbdb559ad
Abdul Basit <abdulbasit10397@gmail.com>
2024-02-19 17:41:19 +0500

    PF19 - File Handling - Writing

--- concepts/filehandling/WritingInFiles.py
+++ concepts/filehandling/WritingInFiles.py
@@ -1,4 +1,6 @@
-# In following snippet, I am over-writing the content of employees.txt with new text
+# In following snippet, I am creating/over-writing the content of employees.txt with new text
+# If the file is already there, it will over-write
+# If the file not there already, it will create a new file and write text into it
 
 employees_text = open("employees.txt", "w")
 employees_text.write("\nPramod - Scrum Master")
