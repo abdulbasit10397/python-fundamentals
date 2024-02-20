2dea3d3ba0f5f1020006b936c45a74ced88d8db8
Abdul Basit <abdulbasit10397@gmail.com>
2024-02-20 06:39:14 +0500

    PF21 - Inheritance

---
+++ concepts/inheritence/ChineseChef.py
@@ -0,0 +1,9 @@
+from inheritence.Chef import Chef
+
+
+class ChineseChef(Chef):
+    def make_special_dish(self):
+        print("The chef makes sushi.")
+
+    def make_noodles(self):
+        print("The chef makes noodles.")
---
+++ concepts/inheritence/MyRestaurant.py
@@ -0,0 +1,9 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+chef1 = Chef()
+chef1.make_special_dish()
+
+chef2 = ChineseChef()
+chef2.make_special_dish()
+chef2.make_noodles()
